d8a60e265c5faed433754772eae00b1665696269
Luís Couto <77895438+luisaraujoc@users.noreply.github.com>
2024-07-08 20:31:37 -0300

    set Role String

--- src/main/java/com/luisaraujoc/cativeriolfc/Enum/Role.java
+++ src/main/java/com/luisaraujoc/cativeriolfc/Enum/Role.java
@@ -2,25 +2,25 @@ package com.luisaraujoc.cativeriolfc.Enum;
 
 public enum Role {
     // 1 - ADMIN, 2 - USER
-    ADMIN(1),
-    USER(2);
+    ADMIN("ADMIN"),
+    USER("USER");
 
-    private final int value;
+    private final String value;
 
-    Role(int value) {
+    Role(String value) {
         this.value = value;
     }
 
-    public int getValue() {
+    public String getValue() {
         return value;
     }
 
-    public static Role fromValue(int value) {
-        for (Role role : Role.values()) {
-            if (role.getValue() == value) {
-                return role;
-            }
-        }
-        throw new IllegalArgumentException("Valor desconhecido: " + value);
-    }
+    // public static Role fromValue(int value) {
+    //     for (Role role : Role.values()) {
+    //         if (role.getValue() == value) {
+    //             return role;
+    //         }
+    //     }
+    //     throw new IllegalArgumentException("Valor desconhecido: " + value);
+    // }
 }
