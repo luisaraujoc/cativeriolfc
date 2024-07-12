ebd8033b550f38bbe3471f8796e25acc1951a72b
Lucas Eduardo <lesds2001@gmail.com>
2024-07-11 21:31:40 -0300

    trabalhando na geração de times

--- src/main/java/com/luisaraujoc/cativeriolfc/daoTester.java
+++ src/main/java/com/luisaraujoc/cativeriolfc/daoTester.java
@@ -1,13 +1,41 @@
 package com.luisaraujoc.cativeriolfc;
 
+import com.luisaraujoc.cativeriolfc.Entity.GameDay;
 import com.luisaraujoc.cativeriolfc.Entity.Person;
+import com.luisaraujoc.cativeriolfc.Entity.Team;
 import com.luisaraujoc.cativeriolfc.Enum.Role;
 import com.luisaraujoc.cativeriolfc.Dao.DaoFactory;
 import com.luisaraujoc.cativeriolfc.Dao.PersonDao;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class daoTester {
-    public static void main(String[] args) {
-        PersonDao pDao = DaoFactory.createPersonDao();
-        pDao.delete(10L);
+    public static void main(String[] args) throws ParseException {
+        List<Person> pList = new ArrayList<Person>();
+        PersonDao pdao = DaoFactory.createPersonDao();
+        List<Person> persons = pdao.findAll();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+
+        for (int i = 0; i < 25; i++) {
+            pList.add(persons.get(i));
+        }
+        Date date = sdf.parse("11/07/2024");
+        GameDay gd = new GameDay(4, date, pList);
+
+        System.out.println(gd);
+
+        List<Team> teams = gd.generateTeams();
+
+        for (Team team : teams) {
+            System.out.println(team);
+        }
+
+
+
     }
 }
